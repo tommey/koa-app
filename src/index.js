cee65f61af65a021be76fabb600705d033594225
Tamas Toth <tommey@tommey.hu>
2015-08-25 09:40:31 -0400

    Added koa-handlebars, a layout and a view for rendering with the template engine

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 var koa = require('koa');
+var handlebars = require('koa-handlebars');
+var Router = require('koa-router');
 
 var app = koa();
+var router = Router();
+
+var requests = 0;
+
+router.get('/', function* (next) {
+	this.body = 'hello #' + requests;
+});
+
+router.get('/ddd', function* (next) {
+        this.body = 'hello /ddd';
+});
+
+router.get('/user', function* (next) {
+	yield this.render('user');
+});
+
+app.use(function* (next) {
+	requests++;
+	yield next;
+});
+
+app.use(handlebars({
+	defaultLayout: 'main.hbs',
+	layoutsDir: 'src/layouts',
+	viewsDir: 'src/views'
+}));
+
+app.use(router.routes());
 
 module.exports = app;
 
