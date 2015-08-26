e39abe50acb4b0dcd823e5671ae237bc7a7ef167
Tamas Toth <tommey@tommey.hu>
2015-08-26 05:25:42 -0400

    Moved routing part to separate file

--- src/index.js
+++ src/index.js
@@ -4,44 +4,11 @@ var handlebars = require('koa-handlebars');
 var Router = require('koa-router');
 var mongoose = require('mongoose');
 var config = require('../config');
+var router = require('./router');
 
 var app = koa();
 
 mongoose.connect(config.mongoDB);
-
-var User = require('./models/user');
-
-var router = Router();
-router.get('/', function* (next) {
-	this.body = 'hello #' + requests;
-});
-
-router.get('/ddd', function* (next) {
-        this.body = 'hello /ddd';
-});
-
-router.get('/user/:id', function* (next) {
-	this.body = yield User.find({_id: this.params.id});
-});
-
-router.post('/user', function* (next) {
-        var user = new User({
-		name: this.request.body.name,
-		age: this.request.body.age
-	});
-
-	this.body = yield user.save();
-});
-
-router.del('/user/:id', function* (next) {
-        yield User.remove({_id: this.params.id});
-
-	this.status = 204;
-});
-
-router.get('/users', function* (next) {
-	this.body = yield User.find();
-});
 
 var requests = 0;
 app.use(function* (next) {
